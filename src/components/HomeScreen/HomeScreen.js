97dcb8015d92333fc7e701f7dafcdcc2b630dda2
Verrya <evan_lefevre@orange.fr>
2023-01-27 12:03:27 +0100

    Create Task component

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,10 +1,13 @@
-import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "../Task/Task";
 
 export default function HomeScreen() {
     return (
         <View>
         <Text>HomeScreen</Text>
+        <Task name="Tâche 1" />
+        <Task name="Tâche 2" />
+        <Task name="Tâche 3" />
         </View>
     );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,31 @@
+import { CheckBox } from "@rneui/themed";
+import { useState } from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+
+export default function Task(props) {
+
+    const [checked, setChecked] = useState(false);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() => {
+                alert("Task pressed");
+            }}>
+                <Text>{props.name ? props.name : "Tâche"}</Text>
+            </TouchableOpacity>
+            <CheckBox checked={checked} onPress={() => {
+                setChecked(!checked);
+            }}>
+            </CheckBox>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+});
