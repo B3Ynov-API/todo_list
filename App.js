ce7136fd0f6b1671f4a9fa4ccf2861b0564f2c39
Verrya <evan_lefevre@orange.fr>
2023-02-17 10:02:15 +0100

    Add icons to tab navigation

--- App.js
+++ App.js
@@ -3,13 +3,28 @@ import { StyleSheet } from 'react-native';
 import { HomeStackScreen, DailyStackScreen } from './src/components/';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faHouse, faCalendar } from '@fortawesome/free-solid-svg-icons'
+
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: () => {
+          let icon;
+
+          if (route.name === 'HomeStack') {
+            icon = faHouse;
+          } else if (route.name === 'DailyStack') {
+            icon = faCalendar;
+          }
+          return <FontAwesomeIcon icon={icon} />;
+        }
+      })}>
         <Tab.Screen name="HomeStack" component={HomeStackScreen} />
         <Tab.Screen name="DailyStack" component={DailyStackScreen} />
       </Tab.Navigator>
